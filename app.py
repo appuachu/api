d5a7de6bb48a06e6f7edad25e5dd0eadff2bc069
Achu <87751449+appuachu@users.noreply.github.com>
2026-05-19 15:18:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ def debug_users():
     return jsonify(users)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5003)
